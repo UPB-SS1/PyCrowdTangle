d9f0445d1333a7a98eb7396b853d74581085f840
Jose R. Zapata <jjrzg@hotmail.com>
2020-06-26 12:55:33 -0500

    add setup files

--- setup.py
+++ setup.py
@@ -6,19 +6,21 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["requests>=2"]
-
 setup(
     name="PyCrowdTangle",
+    packages = ["PyCrowdTangle"],
     version="0.0.1",
     author="Jose R. Zapata",
     author_email="jjrzg@hotmail.com",
+    url = 'https://joserzapata.github.io/',
     description="A Python Wrapper To Retrieve Data From The CrowdTangle API",
+    license='MIT',
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/UPB-SS1/PyCrowdTangle",
-    packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        'request'         
+       ],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
